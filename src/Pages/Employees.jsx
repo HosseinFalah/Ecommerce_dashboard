ee5bf27466b4e540d8ed76daee55a440877af645
HosseinFalah <hosseinfalah2021@gmail.com>
2022-06-01 13:31:49 +0430

    complete component Employees add Purple In Component

--- src/Pages/Employees.jsx
+++ src/Pages/Employees.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Inject, Toolbar } from '@syncfusion/ej2-react-grids';
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../Components';
+import { v4 as uuidv4 } from 'uuid';
 
-function Employees() {
+const Employees = () => {
   return (
-    <div>Employees</div>
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees"/>
+      <GridComponent dataSource={employeesData} allowPaging allowSorting toolbar={['Search']} width="auto">
+        <ColumnsDirective>
+          {employeesGrid.map((item) => (
+            <ColumnDirective key={uuidv4()} {...item}/>
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search, Toolbar]}/>
+      </GridComponent>
+    </div>
   )
 }
 
