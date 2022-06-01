3a849d7db1020a606e2d4d7f3d80a4eab53a14b8
HosseinFalah <hosseinfalah2021@gmail.com>
2022-06-01 14:26:22 +0430

    Add Component Customers Purple && Add feature in Customers , Add || Edit, || Delete

--- src/Pages/Customers.jsx
+++ src/Pages/Customers.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
+import { customersData, customersGrid } from '../data/dummy';
+import Header from './../Components/Header';
+import { v4 as uuidv4 } from 'uuid';
 
-function Customers() {
+const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Customers"/>
+      <GridComponent 
+        dataSource={customersData} 
+        allowPaging 
+        allowSorting 
+        toolbar={['Delete', "Add", "Edit", "Search"]} 
+        editSettings={{allowDeleting: true, allowEditing: true, allowAdding: true}}
+        width="auto">
+        <ColumnsDirective>
+          {customersGrid.map((item) => (
+            <ColumnDirective key={uuidv4()} {...item}/>
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Toolbar, Selection, Edit, Sort, Filter]}/>
+      </GridComponent>
+    </div>
   )
 }
 
-export default Customers
+export default Customers;
