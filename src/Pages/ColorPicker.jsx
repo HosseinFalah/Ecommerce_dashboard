d001d0e1fb2e53e6a6377828663f715f19b3040d
HosseinFalah <98707258+HosseinFalah@users.noreply.github.com>
2022-06-02 10:35:51 +0430

    Add Color Picker in App

--- src/Pages/ColorPicker.jsx
+++ src/Pages/ColorPicker.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
+import { Header } from '../Components';
 
-function ColorPicker() {
+const change = (args) => {
+  document.querySelector("#preview").style.backgroundColor = args.currentValue.hex;
+}
+
+const ColorPicker = () => {
   return (
-    <div>ColorPicker</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="ColorPicker"/>
+      <div className="text-center">
+        <div id="preview"/>
+        <p className="text-2xl font-semibold mt-2 mb-4">Inline Pallete</p>
+        <div className="flex justify-center items-center gap-20 flex-wrap">
+          <ColorPickerComponent
+            id="inline-pallete"
+            mode="Palette"
+            modeSwitcher={false}
+            inline
+            showButtons={false}
+            change={change}
+            />
+          <ColorPickerComponent
+            id="inline-Picker"
+            mode="Picker"
+            modeSwitcher={false}
+            inline
+            showButtons={false}
+            change={change}
+            />
+        </div>
+      </div>
+    </div>
   )
 }
 
